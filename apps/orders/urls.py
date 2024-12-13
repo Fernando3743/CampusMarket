9204af452c6b9f3b4d31c816bc9e01bb978fcabb
Juan Manuel <juan.vargas.munoz@correounivalle.edu.co>
2024-12-13 00:59:15 -0500

    Eliminar y agregar productos al carrito solo cambio en view y urls de orders

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MyOrderView, CreateOrderProductView
+from .views import MyOrderView, CreateOrderProductView, DeleteOrderProductView
 
 urlpatterns = [
     path("mi-orden", MyOrderView.as_view(), name="my_order"),
-    path("agregar-producto", CreateOrderProductView.as_view(), name="add_product"),
+    path("new_order", CreateOrderProductView.as_view(), name="new_order"),
+    path("delete_product", DeleteOrderProductView.as_view(), name='delete_order_product'),
 ]
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import Http404
 
 # Create your views here.
 
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-from .models import Order
+from .models import Order, OrderProduct
 from .forms import OrderProductForm
 
 
@@ -20,7 +21,6 @@ class MyOrderView(LoginRequiredMixin, DetailView):
 
 
 class CreateOrderProductView(LoginRequiredMixin, CreateView):
-    template_name = "orders/create_order_product.html"
     form_class = OrderProductForm
     success_url = reverse_lazy("my_order")
 
@@ -33,3 +33,27 @@ class CreateOrderProductView(LoginRequiredMixin, CreateView):
         form.instance.quantity = 1
         form.save()
         return super().form_valid(form)
+
+class DeleteOrderProductView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        # Obtener el ID del objeto OrderProduct a eliminar
+        order_product_id = request.POST.get('order_product_id')
+        print(order_product_id)
+        
+        if not order_product_id:
+            return redirect('my_order')  # Si no se pasa un id, redirige de vuelta
+
+        try:
+            # Buscar el objeto OrderProduct por su ID y asegurarnos que pertenece al usuario
+            order_product = OrderProduct.objects.get(
+                id=order_product_id,
+                order__user=request.user,  # Aseguramos que el producto pertenece al usuario
+                order__is_active=True       # Solo eliminamos si la orden está activa
+            )
+            # Eliminar el objeto encontrado
+            order_product.delete()
+        except OrderProduct.DoesNotExist:
+            # Si no se encuentra el producto, lanzamos un error 404 o simplemente no hacemos nada
+            raise Http404("Producto no encontrado.")
+            
+        return redirect('my_order')  # Redirigir de vuelta a la vista de la orden
